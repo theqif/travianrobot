34b87ad73979e9ee15f09b6cb2a128a33b667edf
mansionhouseprojects <mansionhouseprojects@7ec92703-de41-0410-8c61-f56f41f81d8f>
2007-12-29 11:42:09 +0000

    with logged_in method

--- scripts/scout.pl
+++ scripts/scout.pl
@@ -14,7 +14,9 @@ my $village_id = shift;
 
 die "usage: $0 [server] [user] [pass] [x] [y] [village_id]\n" unless $server && $user && $pass && $x && $y;
 
-my $USERAGENT = 'Mozilla/5.0 (X11; U; Linux x86_64; en-GB; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11';
+#my $USERAGENT = 'Mozilla/5.0 (X11; U; Linux x86_64; en-GB; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11';
+my $USERAGENT = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en-US; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11";
+
 
 my $travian = Travian->new($server, agent => $USERAGENT);
 if (!$travian->login($user, $pass))
