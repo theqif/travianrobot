4584691e962c8efd0d1c034c95030d8f3e23209e
mansionhouseprojects <mansionhouseprojects@7ec92703-de41-0410-8c61-f56f41f81d8f>
2008-01-13 22:09:51 +0000

    atak reports added ... also new data & new atak_mgt script

--- trunk/scripts/atak_mgt.pl
+++ trunk/scripts/atak_mgt.pl
@@ -1,19 +1,60 @@
+#!/usr/bin/perl -w
 
 use lib '../lib';
-use strict;
-use Travian;
+use Travian (qw/calc_traveltime/);
+use Travian::Troops::Gauls;
+use Carp;
+use Data::Dumper;
+
+my $server = shift;
+my $user = shift;
+my $pass = shift;
+
+my $t = Travian->new($server);
+
+croak $t->error_msg() unless ($t->login($user, $pass));
+
+my $returns =
+{
+ '1' => {x=> 3,y=>34,tt=>20,name=>'junglejazza'},
+ '2' => {x=>33,y=>43,tt=>15,name=>'pipjrob'},
+};
 
 while (1)
 {
-  print `date`;
+  my $dt = time;
 
-  # check if $current_time_epoch is > than list of pending ataks.
-   # launch ataks
-   # remove form pending_ataks
-   # add next atak to pending
-  # parse any reports & update launch schedule accordingly
- 
+print "DT - [$dt]\n";
+print Dumper ($returns);
 
+  foreach my $rt (keys %{$returns})
+  {
+    if ($rt < $dt)
+    {
+      print "\t\tCOOL - time to raid again .. \n";
+      my $ap = $returns->{$rt};
+      my $return = &launch_atak ($t,$ap);
 
-  `sleep 30`;
+      $returns->{$return} = $ap;
+      delete $returns->{$rt};
+    }
+    else { print "Still waiting for " . $returns->{$rt}->{name} . "to get back\n"; }
+  }
+
+  sleep 10;
+}
+
+sub launch_atak
+{
+  my $t  = shift;
+  my $ap = shift;
+
+  my $troop = Travian::Troops::Gauls->new();
+     $troop->theutates_thunder($ap->{tt});
+
+#  croak $t->error_msg() unless ($t->send_troops(Travian::ATTACK_RAID, $ap->{x}, $ap->{y}, $troop));
+
+  my $dt = time;
+  my $ttime = int(&calc_traveltime(19,40, $ap->{x}, $ap->{y}, 19));
+  return ($dt + $ttime + $ttime + 10);
 }
---
+++ trunk/scripts/tq_store_atak_reports.pl
@@ -0,0 +1,126 @@
+#!/usr/bin/perl -w
+
+use lib '../lib/';
+use Travian;
+use Carp;
+
+
+my $server = shift;
+my $user = shift;
+my $pass = shift;
+
+#my $ATTACK_REPORTS_PATH = '/home/ade/Development/perl/travianrobot/reports/attacks/';
+my $ATTACK_REPORTS_PATH = "/Users/qif/travian/travianrobot/reports/attacks/$user/";
+
+die "usage: $0 [server] [user] [pass]\n" unless $server && $user && $pass;
+
+my $travian = Travian->new($server);
+if (!$travian->login($user, $pass))
+{
+	croak $travian->error_msg();
+}
+
+my $report_attacks = $travian->report_headers(Travian::REPORT_ATTACKS);
+while (@{$report_attacks})
+{
+	my @report_ids = ();
+
+	foreach my $header (@{$report_attacks})
+	{
+		my $report = $travian->report($header->id());
+		croak "Cannot retrieve report " . $header->id() . "." unless ($report);
+
+		if ($report->attacker()->name() eq $user)
+		{
+			print 'Saving (' . $header->id() . ') ' . $report->defender()->village() . ' ... ';
+			if (&save_attack($report))
+			{
+				print "Success.\n";
+				my $header_id = $header->id();
+				push(@report_ids, $header_id);
+			}
+			else { print "Failed.\n"; }
+		}
+	}
+
+	$travian->delete_reports(@report_ids);
+
+	$report_attacks = $travian->report_headers(Travian::REPORT_ATTACKS);
+}
+
+sub save_attack
+{
+	my $report = shift;
+
+	my $filename = $ATTACK_REPORTS_PATH . $report->defender()->village() . '.tab';
+	my @report_lines = &read_report_file($filename);
+
+	my $new_report_line = &convert_date($report->header()->sent()) . "\t" .
+				$report->attacker->resources()->wood() . "\t" .
+				$report->attacker->resources()->clay() . "\t" .
+				$report->attacker->resources()->iron() . "\t" .
+				$report->attacker->resources()->wheat() . "\n";
+
+	push(@report_lines, $new_report_line);
+	@report_lines = sort(@report_lines);
+
+	return &write_report_file($filename, @report_lines);
+}
+
+sub convert_date
+{
+	my $sent = shift;
+
+	#on 11/01/08 at 07:54:19 pm
+
+	$sent =~ m#^on (\d+?)/(\d+?)/(\d+?) at (\d+?):(\d+?):(\d+) (.m)$#msgi;
+	my $day = $1;
+	my $month = $2;
+	my $year = $3;
+	my $hour = $4;
+	my $min = $5;
+	my $sec = $6;
+	my $ampm = $7;
+
+	#print "$day $month $year $hour $min $sec $ampm\n";
+
+	if ($ampm eq 'pm') { $hour += 12; }
+	if ($ampm eq 'am' && $hour == 12) { $hour = '00'; }
+
+	return "$day/$month/$year $hour:$min:$sec";	
+}
+
+sub read_report_file
+{
+	my $filename = shift;
+	my @lines = ();
+
+	if (-w $filename)
+	{
+		if (open(REPORT, $filename))
+		{
+			@lines = <REPORT>;
+
+			close(REPORT);
+		}
+	}
+
+	return @lines;
+}
+
+sub write_report_file
+{
+	my $filename = shift;
+	my @lines = @_;
+
+	if (open(REPORT, ">$filename"))
+	{
+		print REPORT @lines;
+		close(REPORT);
+
+		return 1;
+	}
+
+	return;
+}
+
