7f0866878c1eb44d3ab2fa46ec55db1794794eea
mansionhouseprojects <mansionhouseprojects@7ec92703-de41-0410-8c61-f56f41f81d8f>
2008-01-14 22:31:46 +0000

    updated atak mgt script

--- trunk/scripts/atak_mgt.pl
+++ trunk/scripts/atak_mgt.pl
@@ -11,36 +11,47 @@ my $user = shift;
 my $pass = shift;
 
 my $t = Travian->new($server);
-
 croak $t->error_msg() unless ($t->login($user, $pass));
 
 my $attacks =
 {
- '1' => {x=> 3,y=>34,tt=>20,name=>'junglejazza'},
- '2' => {x=>33,y=>43,tt=>15,name=>'pipjrob'},
+ '1200326970' => { tid=> 1, x=> 3, y=>34, tt=>20, name=>'junglejazza', },
+ '2' => { tid=> 2, x=>33, y=>43, tt=>15, name=>'pipjrob',     },
+ '3' => { tid=> 3, x=>22, y=>30, tt=>15, name=>'Aimee',       },
+ '4' => { tid=> 4, x=>13, y=>46, tt=>15, name=>'Bomster',     },
+ '5' => { tid=> 5, x=>10, y=>48, tt=>10, name=>'garner',      },
+ '6' => { tid=> 6, x=> 7, y=>41, tt=> 5, name=>'Death',       },
+ '7' => { tid=> 7, x=>14, y=>37, tt=>10, name=>'madeddy',     },
+ '8' => { tid=> 8, x=>38, y=>49, tt=>15, name=>'naynay123',   },
+ '9' => { tid=> 9, x=>28, y=>49, tt=>35, name=>'scarmace',    },
 };
 
 while (1)
 {
   my $dt = time;
 
-print "DT - [$dt]\n";
-#print Dumper ($attacks);
+print "$0 : [$dt]\n";
 
-  foreach my $rt (keys %{$attacks})
+  foreach my $rt (sort keys %{$attacks})
   {
     if ($rt < $dt)
     {
+      if (!$t->logged_in)
+      {
+        warn "***loggin in again";
+        croak $t->error_msg() unless ($t->login($user, $pass));
+      }
+
+
+
       my $rep = &find_atak_report($t, $user, $attacks->{$rt}->{name});
+      warn "***Can't find report of previous attack" unless ($rep);
 
-      warn "Can't find report of previous attack" unless ($rep);
-
-#$attacks->{$rt}->{tt} += 5 if (&is_max($rep));
+      if ($rep)
+      {
+        if ($rep->is_max) { print "MAX!! - increase?\n" } else { print "Not max - decrease?\n"; }
 #$attacks->{$rt}->{tt} += 5 if ($rep->is_max);
-#print Dumper ($attacks->{$rt});
-#sub is_max { return 0; }
-
-      if ($rep->is_max) { print "MAX!! - increase?\n" } else { print "Not max - decrease?\n"; }
+      }
 
       print "\t\tTime to raid [" . $attacks->{$rt}->{name} . "] again .. \n";
       my $return = &launch_atak ($t,$attacks->{$rt});
@@ -51,10 +62,10 @@ print "DT - [$dt]\n";
       $attacks->{$return} = $attacks->{$rt};
       delete $attacks->{$rt};
     }
-    else { print "Still waiting for [" . $attacks->{$rt}->{name} . "] to get back\n"; }
+    else { print "\t" . ($rt-$dt) . " secs until [" . $attacks->{$rt}->{name} . "] returns\n"; }
   }
 
-  sleep 10;
+  sleep 60;
 }
 
 sub launch_atak
@@ -65,11 +76,16 @@ sub launch_atak
   my $troop = Travian::Troops::Gauls->new();
      $troop->theutates_thunder($ap->{tt});
 
-#  croak $t->error_msg() unless ($t->send_troops(Travian::ATTACK_RAID, $ap->{x}, $ap->{y}, $troop));
+  $t->village('74134');
+
+  if (!$t->send_troops(Travian::ATTACK_RAID, $ap->{x}, $ap->{y}, $troop))
+  {
+    warn "***".$t->error_msg;
+    return 0;   
+  }
 
   my $dt = time;
   my $ttime = int(&calc_traveltime(19,40, $ap->{x}, $ap->{y}, 19));
-return $dt + 30;
   return ($dt + $ttime + $ttime + 10);
 }
 
@@ -82,7 +98,7 @@ sub find_atak_report
   my $target = shift || return undef;
   my $i      = shift || 0;
 
-print "Searching for reports, dtarting at [$i]\n";
+print "\t\tSearching for [$target] reports, starting at [$i]\n";
 
   my $report_attacks = $t->report_headers(Travian::REPORT_ATTACKS,$i);
 
@@ -93,14 +109,12 @@ print "Searching for reports, dtarting at [$i]\n";
 
     croak "Cannot retrieve report " . $header->id() . "." unless ($r);
 
-    print "atak : [" . $r->attacker->name . "]\tdefs : [" . $r->defender->name . "]\n";
-
     next unless ($r->attacker()->name() eq $user);
     next unless ($r->defender()->name() eq $target);
 
     return $r;
   }
-print "i - [$i]\n\n\n";
+print "\t\t\ti - [$i]\n\n\n";
   if ($i % 10 == 0) { return &find_atak_report($t,$user,$target,$i); }
   return undef;
 }
