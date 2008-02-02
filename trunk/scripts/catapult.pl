b457fe5e191011c4f4d9d4343135ef947a278d0d
aelgar <aelgar@7ec92703-de41-0410-8c61-f56f41f81d8f>
2008-02-02 06:30:15 +0000

    added catapult target selection to send_troops() method.

---
+++ trunk/scripts/catapult.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w
+
+use lib '../lib/';
+use Travian;
+use Travian::Troops::Gauls;
+use Carp;
+
+my $server = shift;
+my $user = shift;
+my $pass = shift;
+my $x = shift;
+my $y = shift;
+my $village_id = shift;
+
+die "usage: $0 [server] [user] [pass] [x] [y] [village_id]\n" unless $server && $user && $pass && $x && $y;
+
+#my $USERAGENT = 'Mozilla/5.0 (X11; U; Linux x86_64; en-GB; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11';
+my $USERAGENT = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en-US; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11";
+
+
+my $travian = Travian->new($server, agent => $USERAGENT);
+if (!$travian->login($user, $pass))
+{
+	croak $travian->error_msg();
+}
+
+if ($village_id && !$travian->village($village_id))
+{
+	croak $travian->error_msg();
+}
+
+my $gauls = Travian::Troops::Gauls->new();
+$gauls->phalanx(311);
+$gauls->trebuchet(9);
+
+if (!$travian->send_troops(Travian::ATTACK_NORMAL, $x, $y, $gauls, Travian::CATA_RANDOM))
+{
+	croak $travian->error_msg();
+}
+
+#$travian->logout();
+
+
