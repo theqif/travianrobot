c1544b99ee49d59090ef7d4c93766d40221096ce
mansionhouseprojects <mansionhouseprojects@7ec92703-de41-0410-8c61-f56f41f81d8f>
2008-02-16 06:57:05 +0000

    few changes including screen res

--- trunk/scripts/profile.pl
+++ trunk/scripts/profile.pl
@@ -15,6 +15,7 @@ my $USERAGENT = 'Mozilla/5.0 (X11; U; Linux x86_64; en-GB; rv:1.8.1.11) Gecko/20
 #my $USERAGENT = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en-US; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11";
 
 my $travian = Travian->new($server, agent => $USERAGENT);
+$travian->proxy('http', 'http://localhost/');
 
 if (!$travian->login($user, $pass))
 {
